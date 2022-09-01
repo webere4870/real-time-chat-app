5644f4298f845d0b2085b6704867e57706a28897
Eli Weber <eliweber2001@gmail.com>
2022-09-01 08:48:00 -0400

    Socket bugs refactored

--- Socket.js
+++ Socket.js
@@ -2,6 +2,8 @@ let SaveMessage = require('./utils/SaveMessage')
 let UserModel = require('./MongoDB/Schema')
 let fetch = require('node-fetch')
 let currentUser = null
+let currentRoom = null
+let MemoryUserList = []
 
 
 module.exports = (server)=>
@@ -13,32 +15,30 @@ module.exports = (server)=>
 
         socket.on("userStatus", (statusObject)=>
         {
-            console.log(statusObject)
             socket.to(statusObject.room).emit("userStatus", {user: statusObject.currentUser, active: statusObject.active})
         })
         
-        socket.on("username", (username)=>
+        socket.on("username", async (username)=>
         {
-            currentUser = username
-            console.log(currentUser, "user")
-            socket.on("disconnect", async ()=>
-            {
-                console.log(socket.id + " has disconnected")
-                
-                let response = await UserModel.updateOne({_id: username}, {$set: {active: false}})
-            })
+            MemoryUserList.push({id: socket.id, email: username, roomList: []})
+            await UserModel.updateOne({_id: username}, {$set: {active: true}})
         })
 
        
         let room = null
-        console.log(socket.id + " has connected to the server")
 
 
-        socket.on("newRoom", (roomName)=>
+        socket.on("newRoom", async (roomName)=>
         {
-            console.log(socket.id + " joined " + roomName)
+            for(let temp of MemoryUserList)
+            {
+                if(temp.id == socket.id)
+                {
+                    temp.roomList.push(roomName)
+                }
+            }
             socket.join(roomName)
-            room = roomName
+            console.log(MemoryUserList)
         })
 
         socket.on("roomMessage", async (messageProfile)=>
@@ -46,6 +46,19 @@ module.exports = (server)=>
             messageProfile.room = room
             let newMessage = await SaveMessage(messageProfile)
             io.to(room).emit("roomMessage", newMessage)
+        })
+
+        socket.on("disconnect", async ()=>
+        {
+            console.log(socket.id + " has disconnected")
+            let newIndex = MemoryUserList.findIndex((temp)=> temp.id == socket.id)
+            let response = await UserModel.updateOne({_id: MemoryUserList[newIndex].email}, {$set: {active: false}})
+            for(let temp of MemoryUserList[newIndex].roomList)
+            {
+                socket.to(temp).emit("userStatus", {user: MemoryUserList[newIndex].email, active: false})
+            }
+            const index = MemoryUserList.findIndex((temp)=>temp.id === socket.id)
+            MemoryUserList.splice(index, 1)
         })
     })
 }
--- public/chatScript.js
+++ public/chatScript.js
@@ -10,6 +10,23 @@ socket.on("getUsername", async (evt)=>
     let json = await username.json()
     console.log(json.email)
     socket.emit("username", json.email)
+    
+
+    let userList = fetch('/userList').then(async (result)=>
+    {
+        let newRes = await result.json()
+        roomList = newRes.activityList
+        currentUser = newRes.username
+        for(let temp of roomList)
+        {
+            console.log("here room")
+            socket.emit("newRoom", temp)
+        }
+        for(let temp of roomList)
+        {
+            socket.emit("userStatus", {currentUser: currentUser, room: temp, active: true})
+        }
+    })
 })
 let changeActivity = fetch("/changeActivity?active=true").then((email)=>
 {
@@ -22,35 +39,25 @@ let changeActivity = fetch("/changeActivity?active=true").then((email)=>
 
 socket.on("userStatus", (statusObject)=>
 {
-    console.log("here")
+    console.log(statusObject)
     if(statusObject.active == true)
     {
-        $(`li[data-id="${statusObject.user}"] span`).attr("class", "status green")
-        $(`li[data-id="${statusObject.user}"] .statusText`).text("Online")
+        console.log("Active")
+        $('li').attr("data-id", statusObject.user).find("span").attr("class", "status green")
+        $('li').attr("data-id", statusObject.user).find(".statusText").text("Online")
+        // $(`li[data-id="${statusObject.user}"] span`).attr("class", "status green")
+        // $(`li[data-id="${statusObject.user}"] .statusText`).text("Online")
     }
     else
     {
-        $(`li[data-id=${statusObject.user}] span`).attr("class", "status orange")
-        $(`li[data-id=${statusObject.user}] .statusText`).text("Offline")
+        console.log("Inavtive")
+        $('li').attr("data-id", statusObject.user).find("span").attr("class", "status orange")
+        $('li').attr("data-id", statusObject.user).find(".statusText").text("Offline")
+        // $(`li[data-id=${statusObject.user}] span`).attr("class", "status orange")
+        // $(`li[data-id=${statusObject.user}] .statusText`).text("Offline")
     }
 })
 
-
-let userList = fetch('/userList').then(async (result)=>
-{
-    let newRes = await result.json()
-    roomList = newRes.activityList
-    currentUser = newRes.username
-    for(let temp of roomList)
-    {
-        console.log("here room")
-        socket.emit("newRoom", temp)
-    }
-    for(let temp of roomList)
-    {
-        socket.emit("userStatus", {currentUser: currentUser, room: temp, active: true})
-    }
-})
 
 
 
